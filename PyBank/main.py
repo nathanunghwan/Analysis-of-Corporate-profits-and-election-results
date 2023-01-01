af6c6c7dd296667fa3b441ec45b8d217598f8e57
nathanunghwan <nathanahn03@gmail.com>
2023-01-01 00:50:11 -0500

    first one clear

--- PyBank/main.py
+++ PyBank/main.py
@@ -2,89 +2,38 @@ import os
 import csv
 
 csv_path =os.path.join("Resources","budget_data.csv")
-print(csv_path)
+
 with open (csv_path) as csv_file:
     csv_reader = csv.reader(csv_file, delimiter =",")
 
     csv_header = next(csv_reader)
     #print(f'CSV Header: {csv_header}')
     
-    a=dict(csv_reader)
-    b=list(a.keys())
-    c=list(map(int,a.values()))
+    dic_csv = dict(csv_reader)
+    key_csv = list(dic_csv.keys())
+    value_csv = list(map(int,dic_csv.values()))
     
-    net_total_amount = sum(c)
-    total_months = len(b)
-    change = [0] + [ (c[i] - c[i-1]) for i in range(1,total_months)]
-    avg_change=sum(change)/(len(change)-1)
-    max_increase=max(change)
-    date_increase=b[change.index(max_increase)]
-    max_decrease=min(change)
-    date_decrease=b[change.index(max_decrease)]
+    net_total_amount = sum(value_csv)
+    total_months = len(key_csv)
+    change = [0] + [ (value_csv[i] - value_csv[i-1]) for i in range(1,total_months)]
+    avg_change = sum(change)/(len(change)-1)
+    max_increase = max(change)
+    date_increase = key_csv[change.index(max_increase)]
+    max_decrease = min(change)
+    date_decrease = key_csv[change.index(max_decrease)]
 
-    #print(a)
-    #print(b)
-    #print(c)
-    print(f'Total Month: {total_months}')
-    print(f'Total: ${net_total_amount}')
-    
-    print(f'Average Change: ${round(avg_change,2)}')
-    print(f'Greatest Increase in Profits: {date_increase} (${max_increase})')
-    print(f'Greatest Decrease in Profits: {date_decrease} (${max_decrease})')
+with open ("result.txt",'w') as file_csv:
+    contents=['Financial Analysis\n','-'*30,
+              f'\nTotal Month: {total_months}',
+              f'\nTotal: ${net_total_amount}',
+              f'\nAverage Change: ${round(avg_change, 2)}',
+              f'\nGreatest Increase in Profits: {date_increase} (${max_increase})',
+              f'\nGreatest Decrease in Profits: {date_decrease} (${max_decrease})']
 
+    for j in range(len(contents)):
+        print(contents[j])
+        print(contents[j],file=file_csv)
 
-    '''
-    a= list(csv_reader)
-    prof_loss=list(int(profit[1]) for profit in a)
-    fiscal_date = [fiscal[0] for fiscal in a]
-    net_total_amount = sum(prof_loss)
-    total_months = len(fiscal_date)
-    change = [ (prof_loss[i] - prof_loss[i-1]) for i in range(1,total_months)]
-    average_change= sum(change)/len(change)
-    #print(list(csv_reader))
-    print(fiscal_date )
-    print(prof_loss)
-    print(total_months)
-    print(net_total_amount)
-    print(average_change)
-    '''
-    '''
-    for i in range(1,len(net_total_amount)):
-        a=net_total_amount[i]
-        b=net_total_amount[i+1]
-        a, b = b, b-a
-
-'''
-
-
-   
-    '''
-    sum=0
-    count=0
-    for row in csv_reader:
-        count +=1
-        sum += int(row[1])
-
-
-
-    #print(b)
-
-    print(c)
-    print(sum(c))
-    print(len(c))
-    print(max(c))
-    #print(sum(a.values()))
-    Total_Months =0
-    Total = 0
-
-    for row in (csv_reader):
-        Total += int(row[1])
-        Total_Months +=1
-
-        #avg=row[1]
-    print(Total_Months)
-    print(Total)
-'''
 
 
 
