c1fda9916eb86cb898a6cc1b9e2532e2d85215e2
nathanunghwan <nathanahn03@gmail.com>
2023-01-03 00:03:49 -0500

    comment revise

--- PyBank/main.py
+++ PyBank/main.py
@@ -7,19 +7,21 @@ with open (csv_path) as csv_file:
     csv_reader = csv.reader(csv_file, delimiter =",")
     # Header
     csv_header = next(csv_reader)
-    # print(f'CSV Header: {csv_header}')
     # Connvert Dict type
     dic_csv = dict(csv_reader)
     # Extract keys and convert to list ----"Month"
     key_csv = list(dic_csv.keys())
+    #print(key_csv) -->['Jan-10',~~, 'Feb-17']
     # Extract values and convert to list(int type) ---"Profit/loss"
     value_csv = list(map(int,dic_csv.values()))
+    #print(value_csv) -->[1088983, -354534,~~ , 607208, 382539]
     # Total Profit/Loss
     net_total_amount = sum(value_csv)
     # Total Months
     total_months = len(key_csv)
     # Profit loss monthly change,The first month = 0
     change = [0] + [(value_csv[i] - value_csv[i-1]) for i in range(1,total_months)]
+    #print(change)  -->[0, -1443517, ~~, 90895, -224669]
     # Average Change of profit and loss
     avg_change = sum(change)/(len(change)-1)
     # Greatest increase in profit
