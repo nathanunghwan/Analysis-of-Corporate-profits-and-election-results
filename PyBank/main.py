adb9c503aab599f14d9a30b882b080a7c0b0cb3f
nathanunghwan <nathanahn03@gmail.com>
2023-01-03 18:18:13 -0500

    Easy word

--- PyBank/main.py
+++ PyBank/main.py
@@ -8,30 +8,30 @@ with open (csv_path) as csv_file:
     # Header
     csv_header = next(csv_reader)
     # Connvert Dict type
-    dic_csv = dict(csv_reader)
+    dict_csv = dict(csv_reader)
     # Extract keys and convert to list ----"Month"
-    key_csv = list(dic_csv.keys())
+    date_list = list(dict_csv.keys())
     #print(key_csv) -->['Jan-10',~~, 'Feb-17']
     # Extract values and convert to list(int type) ---"Profit/loss"
-    value_csv = list(map(int,dic_csv.values()))
+    profit_list = list(map(int,dict_csv.values()))
     #print(value_csv) -->[1088983, -354534,~~ , 607208, 382539]
     # Total Profit/Loss
-    net_total_amount = sum(value_csv)
+    net_total_amount = sum(profit_list)
     # Total Months
-    total_months = len(key_csv)
+    total_months = len(date_list)
     # Profit loss monthly change,The first month = 0
-    change = [0] + [(value_csv[i] - value_csv[i-1]) for i in range(1,total_months)]
-    #print(change)  -->[0, -1443517, ~~, 90895, -224669]
+    profit_change = [0] + [(profit_list[i] - profit_list[i-1]) for i in range(1,total_months)]
+    #print(profit_change)  -->[0, -1443517, ~~, 90895, -224669]
     # Average Change of profit and loss
-    avg_change = sum(change)/(len(change)-1)
+    avg_change = sum(profit_change)/(len(profit_change)-1)
     # Greatest increase in profit
-    max_increase = max(change)
+    max_increase = max(profit_change)
     # Use index to find that month
-    date_increase = key_csv[change.index(max_increase)]
+    date_increase = date_list[profit_change.index(max_increase)]
     # Greatest decrease in profit
-    max_decrease = min(change)
+    max_decrease = min(profit_change)
     # Use index to find that month
-    date_decrease = key_csv[change.index(max_decrease)]
+    date_decrease = date_list[profit_change.index(max_decrease)]
 
 with open ("analysis/result.txt",'w') as file_csv:
     # What to print out
--- PyPoll/main.py
+++ PyPoll/main.py
@@ -7,25 +7,26 @@ with open (csv_path) as csv_file:
     csv_reader = csv.reader(csv_file, delimiter=',')
     # Header
     csv_header = next(csv_reader)
+
     # extract just candidate and make a list
-    csv_reader_list=[i[2] for i in csv_reader]
-    #print(csv_reader_list)--> ['Diana DeGette',~~ ,'Raymon Anthony Doane']
+    candidate=[i[2] for i in csv_reader]
+    #print(candidate)--> ['Diana DeGette',~~ ,'Raymon Anthony Doane']
     # Total vote
-    total_vote= len(csv_reader_list)
+    total_vote= len(candidate)
     # Use set function, collect name of candidates
-    candidate = list(set(csv_reader_list))
-    #print(candidate) -->['Charles Casper Stockham', 'Raymon Anthony Doane', 'Diana DeGette']
+    name = list(set(candidate))
+    #print(name) -->['Charles Casper Stockham', 'Raymon Anthony Doane', 'Diana DeGette']
     # Candidate name, rate of vote, numbers of vote
-    temp_list= [(candidate[j],
-                 round(csv_reader_list.count(candidate[j])*100/total_vote,3),
-                 csv_reader_list.count(candidate[j]))
-                for j in range(len(candidate))]
-    #print(temp_list)--> [('Diana DeGette', 73.812, 272892), ('Charles Casper Stockham', 23.049, 85213), ('Raymon Anthony Doane', 3.139, 11606)]
+    result= [(name[j],
+                 round(candidate.count(name[j])*100/total_vote,3),
+                 candidate.count(name[j]))
+                for j in range(len(name))]
+    #print(result)--> [('Diana DeGette', 73.812, 272892), ('Charles Casper Stockham', 23.049, 85213), ('Raymon Anthony Doane', 3.139, 11606)]
     # sort by name
-    result_list=sorted(temp_list,key=lambda x: x[0])
-    #print(result_list) -->[('Charles Casper Stockham', 23.049, 85213), ('Diana DeGette', 73.812, 272892), ('Raymon Anthony Doane', 3.139, 11606)]
+    sort_by_name=sorted(result,key=lambda x: x[0])
+    #print(sort_by_name) -->[('Charles Casper Stockham', 23.049, 85213), ('Diana DeGette', 73.812, 272892), ('Raymon Anthony Doane', 3.139, 11606)]
     # sort by vote
-    winner=sorted(temp_list,key=lambda x : -x[2])[0]
+    winner=sorted(result,key=lambda x : -x[2])[0]
     #print(winner) -->('Diana DeGette', 73.812, 272892)
 
 with open("analysis/result_Poll.txt", 'w') as file_csv:
@@ -34,9 +35,9 @@ with open("analysis/result_Poll.txt", 'w') as file_csv:
     # save to txt file
     print('Election Results\n', f"{'-' * 30}\n", f'Total Votes: {total_vote}\n', f"{'-' * 30}\n", sep='\n', file=file_csv)
     # results by candidate
-    for k in range(len(result_list)):
-        print(f'{result_list[k][0]}: {result_list[k][1]}% ({result_list[k][2]})\n')
-        print(f'{result_list[k][0]}: {result_list[k][1]}% ({result_list[k][2]})\n', file=file_csv)
+    for k in range(len(sort_by_name)):
+        print(f'{sort_by_name[k][0]}: {sort_by_name[k][1]}% ({sort_by_name[k][2]})\n')
+        print(f'{sort_by_name[k][0]}: {sort_by_name[k][1]}% ({sort_by_name[k][2]})\n', file=file_csv)
     # display on terminal and  save to txt file
     print(f"{'-' * 30}\n", f'Winner: {winner[0]} \n', f"{'-' * 30}", sep='\n')
     print(f"{'-' * 30}\n", f'Winner: {winner[0]} \n', f"{'-' * 30}", sep='\n', file=file_csv)
